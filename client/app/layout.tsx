dbb0f270dd9acd8bb0e65b9406e88f5be2c590a5
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-06-30 15:30:18 +0530

    initialize next.js client with TypeScript

---
+++ client/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Swiftrobotics",
+  description:
+    "Swiftrobotics is a robotics team that competes in the FIRST Robotics Competition (FRC). We are dedicated to inspiring and educating students in the fields of science, technology, engineering, and mathematics (STEM) through hands-on experience in designing, building, and programming robots.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold text-center mb-8">
+          Welcome to Swiftrobotics
+        </h1>
+      </div>
+    </main>
+  );
+}
